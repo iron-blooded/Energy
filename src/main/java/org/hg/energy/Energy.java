4e9997948f8f81a8016bda7b70ec59e818b5a00c
iron-blooded <ayss2@mail.ru>
2024-09-21 18:17:10 +0500

    start

---
+++ src/main/java/org/hg/energy/Energy.java
@@ -0,0 +1,17 @@
+package org.hg.energy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Energy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
